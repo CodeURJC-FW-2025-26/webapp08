b727e8670e1ddf0d82453bbdf0be61c94d1869de
Ahmed Ehab <ahmed.ehab0676@gmail.com>
2025-12-17 16:53:08 +0100

    Update Ingredient.js

--- Práctica 3/models/Ingredient.js
+++ Práctica 3/models/Ingredient.js
@@ -1,11 +1,11 @@
 // models/Ingredient.js
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const IngredientSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String, default: "" },
-  image: { type: String, default: "" },
-  createdAt: { type: Date, default: Date.now },
+  description: { type: String, default: '' },
+  image: { type: String, default: '' },
+  createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Ingredient", IngredientSchema);
+module.exports = mongoose.model('Ingredient', IngredientSchema);
